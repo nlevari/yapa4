f7d0e5b1f698b7a817f768e1cea889ed05b21543
Nir Lev-Ari <nirla@seematics.com>
2017-06-06 23:56:13 +0300

    another try

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/build'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/build');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('build/index');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
